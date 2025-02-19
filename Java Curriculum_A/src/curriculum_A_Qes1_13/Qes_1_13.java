49c5530682a502c91edc2fbbabc9bda29abfbf3d
tanabetakuonore <tanabetakuonore@localhost>
2025-02-19 13:20:25 +0900

    変更点　①ファイルをまとめました。　②Qes2の初期値を入力しました。

---
+++ Java Curriculum_A/src/curriculum_A_Qes1_13/Qes_1_13.java
@@ -0,0 +1,454 @@
+package curriculum_A_Qes1_13;
+
+public class Qes_1_13 {
+
+	public static void main(String[] args) {
+	
+	//　Qes1
+		
+	//[概要] バイト型の初期化処理
+	//[詳細]　1を初期値に設定する。
+		byte byteNum = 1;
+		System.out.println(byteNum);
+			
+	//[概要]　短整数型の初期化処理
+	//[詳細]　10を初期値に設定する。
+		short shortNum = 10;
+		System.out.println(shortNum);
+				
+	//[概要]　整数型の初期化処理
+	//[詳細]　100を初期値に設定する。
+		int intNum = 100;
+		System.out.println(intNum);
+					
+	//[概要]　長整数型の初期化処理
+	//[詳細]　1000を初期値に設定する。
+		long longNum = 1000;
+		System.out.println(longNum);
+				
+	//[概要]　単精度浮動小数点型の初期化処理
+	//[詳細]　1.5を初期値に設定する。
+		float floatNum = 1.5f;
+		System.out.println(floatNum);
+					
+	//[概要]　倍精度浮動小数点型の初期化処理
+	//[詳細]　1.123を初期値に設定する。
+		double doubleNum = 1.123;
+		System.out.println(doubleNum);
+					
+	//[概要]　文字型の初期化処理
+	//[詳細]　「A」を初期値に設定する。
+		char charWord = 'A';
+		System.out.println(charWord);
+					
+	//[概要]　文字列型の初期化処理
+	//[詳細]　「Hello」を初期値に設定する。
+		String stringWord = "Hello";
+		System.out.println(stringWord);
+						
+	//[概要]　ブーリアン型の初期化処理
+	//[詳細]　trueを初期値に設定する。
+		boolean booleanNum = true;
+		System.out.println(booleanNum);
+		
+		
+		
+	// Qes2
+		
+	//[概要] バイト型の初期化処理
+	//[詳細]　1を初期値に設定する。
+		byte byteNum2 = 0;
+		System.out.println(byteNum2);
+			
+	//[概要]　短整数型の初期化処理
+	//[詳細]　10を初期値に設定する。
+		short shortNum2 = 0;
+		System.out.println(shortNum2);
+			
+	//[概要]　整数型の初期化処理
+	//[詳細]　100を初期値に設定する。
+		int intNum2 = 0;
+		System.out.println(intNum2);
+				
+	//[概要]　長整数型の初期化処理
+	//[詳細]　1000を初期値に設定する。
+		long longNum2 = 0L;
+		System.out.println(longNum2);
+			
+	//[概要]　単精度浮動小数点型の初期化処理
+	//[詳細]　1.5を初期値に設定する。
+		float floatNum2 = 0.0f;
+		System.out.println(floatNum2);
+				
+	//[概要]　倍精度浮動小数点型の初期化処理
+	//[詳細]　1.123を初期値に設定する。
+		double doubleNum2 = 0.0d;
+		System.out.println(doubleNum2);
+				
+	//[概要]　文字型の初期化処理
+	//[詳細]　「A」を初期値に設定する。
+		char charWord2 = '\u0000';
+		System.out.println(charWord2);
+				
+	//[概要]　文字列型の初期化処理
+	//[詳細]　「Hello」を初期値に設定する。
+		String stringWord2 = "null";
+		System.out.println(stringWord2);
+					
+	//[概要]　ブーリアン型の初期化処理
+	//[詳細]　trueを初期値に設定する。
+		boolean booleanNum2 = false;
+		System.out.println(booleanNum2);
+	
+	
+	
+	// Qes3
+	
+	//[概要] バイト型に代入処理
+	//[詳細]　10を代入する。
+		byte byteNum3 = 1;
+		System.out.println(byteNum3);
+	// 10を代入。	
+		byteNum3 = 10;
+		System.out.println(byteNum3);	
+			
+			
+	//[概要]　短整数型に代入処理
+	//[詳細]　100を代入する。
+		short shortNum3 = 10;
+		System.out.println(shortNum3);
+	// 100を代入。
+		shortNum3 = 100;
+		System.out.println(shortNum3);
+			
+			
+	//[概要]　整数型に代入処理
+	//[詳細]　1000を代入する。
+		int intNum3 = 100;
+		System.out.println(intNum3);
+	// 1000を代入。
+		intNum3 = 1000;
+		System.out.println(intNum3);
+				
+			
+	//[概要]　長整数型に代入処理
+	//[詳細]　10000を代入する。
+		long longNum3 = 1000;
+		System.out.println(longNum3);
+	// 10000を代入。
+		longNum3 = 10000;
+		System.out.println(longNum3);
+			
+			
+	//[概要]　単精度浮動小数点型に代入処理
+	//[詳細]　9.5を代入する。
+		float floatNum3 = 1.5f;
+		System.out.println(floatNum3);
+	// 9.5を代入。
+		floatNum3 = 9.5f;
+		System.out.println(floatNum3);
+			
+			
+	//[概要]　倍精度浮動小数点型に代入処理
+	//[詳細]　10.5を代入する。
+		double doubleNum3 = 1.123;
+		System.out.println(doubleNum3);
+	// 10.5を代入。
+		doubleNum3 = 10.5;
+		System.out.println(doubleNum3);
+			
+			
+	//[概要]　文字型に代入処理
+	//[詳細]　「a」を代入する。
+		char charWord3 = 'A';
+		System.out.println(charWord3);
+	// 「A」を代入。
+		charWord3 = 'a';
+		System.out.println(charWord3);
+			
+			
+	//[概要]　文字列型に代入処理
+	//[詳細]　「ハロー」を代入する。
+		String stringWord3 = "Hello";
+		System.out.println(stringWord3);
+	// 「ハロー」を代入。
+		stringWord3 = "ハロー";
+		System.out.println(stringWord3);
+			
+			
+	//[概要]　ブーリアン型に代入処理
+	//[詳細]　trueを代入する。
+		boolean booleanNum3 = false;
+		System.out.println(booleanNum3);
+	// true を代入。
+		booleanNum3 = true;
+		System.out.println(booleanNum3);
+	
+		
+		
+	//Qes4
+		
+	//[概要]　短整数型に代入処理
+	//[詳細]　11110を入力する。
+		short shortNum4 = 100;
+		System.out.println(shortNum4);
+		shortNum4 = 11110;
+		System.out.println(shortNum4);
+		
+	//[概要]　バイト型に代入処理
+	//[詳細]　20を入力する。	
+		byte byteNum4 = 10;
+		System.out.println(byteNum4);
+		byteNum4 = 20;
+		System.out.println(byteNum4);
+		
+	//[概要]　文字型、文字列型、ブーリアン型に初期化処理
+	//[詳細]　それぞれに入力をし、繋ぐ。	
+		char charWord4 = 'a';
+		String stringWord4 = "ハロー";
+		boolean booleanWord4 = true;
+		System.out.println(charWord4 +  " " + stringWord4 + " " + booleanWord4);
+		
+	//[概要]　長整数型に代入処理
+	//[詳細]　10000と1130を代入し、数字をたす。
+		short Num41 = 1000;
+		System.out.println(Num41);
+		Num41 = 10000;
+		short Num4 = 1130;
+		System.out.println(Num41 + Num4);
+			
+	//[概要]　長整数型に代入処理
+	//[詳細]　1000と1000000を代入し、小数点以外の数字をかける。
+		long longNum4 = 1000;
+		System.out.println(longNum4);
+		longNum4 = 10000;
+		long longNum41 = 1000000;
+		System.out.println(longNum4 * longNum41);
+			
+	//[概要]　倍精度浮動小数点型とバイト型に代入処理
+	//[詳細]　10.5と100を代入する。10.5割る100をする。
+		double doubleNum4 = 1.123;
+		System.out.println(doubleNum4);
+		doubleNum4 = 10.5;
+		byte byteNum41 = 100;
+		System.out.println(doubleNum4 / byteNum41);	
+		
+	//[概要] バイト型に代入処理
+	//[詳細]　10と100を代入する。10引く100をする。
+		byte byteNum43 = 1;
+		System.out.println(byteNum43);
+		byteNum43 = 10;
+		byte byteNum42 = 100;
+		System.out.println(byteNum43 - byteNum42);
+			
+	
+		
+	//Qes5
+		
+	//[概要]　修正処理
+	//[詳細]　文字列型、整数型をバイト型に変換する。20についていたダブルクオーテーションを外す。
+		
+		byte num = 20;
+		byte num1 = 23;
+		System.out.println("ハローJAVA"+ (num + num1));
+	
+		
+		
+	//Qes6
+		
+	//[概要]　文字列型に代入処理
+	//[詳細]　「山田太郎」を代入する。
+		String name = "山田太郎"; //名前
+		System.out.println("初めまして" + name + "です");
+				
+	//[概要] バイト型に代入処理
+	//[詳細]　18を代入する。
+		byte old = 18; //年齢
+		System.out.println("年齢は" + old + "歳です");
+			
+	//[概要]　単精度浮動小数点型に代入処理
+	//[詳細]　170.5を代入する。
+		float height = 170.5f; //身長
+		System.out.println("身長は" + height + "cmです");
+					
+	//[概要]　単精度浮動小数点型に代入処理
+	//[詳細]　62.2を代入する。
+		float  weight= 62.2f;  //体重
+		System.out.println("体重は" + weight + "kgです");
+					
+	//[概要]　文字列型に代入処理
+	//[詳細]　「寿司」を代入する。
+		String food = "寿司"; //好きな食べ物
+		System.out.println("好きな食べ物は" + food + "です");		
+	
+		
+		
+	//Qes7	
+		
+	//[概要]　単精度浮動小数点型に代入処理
+	//[詳細]　170.5を代入する。
+		float height7 = 170.5f; //身長
+			
+	//[概要]　単精度浮動小数点型に代入処理
+	//[詳細]　62.2を代入する。
+		float  weight7 = 62.2f; //体重
+			
+	//[概要]　変数を入力し計算処理
+	//[詳細]　BMI＝体重(kg)　÷　身長(m)の2乗　になるように代入する。
+		System.out.println("BMIは" + (weight7 / Math.pow((height7 / 100),2) ) + "です");
+	
+		
+		
+	//Qes8
+		
+	//[概要]　文字列型に代入処理
+	//[詳細]　「鈴木一郎」を代入する。
+		String name8 = "鈴木一郎"; //名前
+		System.out.println("初めまして" + name8 + "です");
+					
+	//[概要] バイト型に代入処理
+	//[詳細]　24を代入する。
+		byte old8 = 24;  //年齢
+		System.out.println("年齢は" + old8 + "歳です");
+					
+	//[概要]　単精度浮動小数点型に代入処理
+	//[詳細]　168.5を代入する。
+		float height8 = 168.5f; //身長
+		System.out.println("身長は" + height8 + "cmです");
+					
+	//[概要]　単精度浮動小数点型に代入処理
+	//[詳細]　64.2を代入する。
+		float  weight8 = 64.2f; //体重
+		System.out.println("体重は" + weight8 + "kgです");
+					
+	//[概要]　文字列型に代入処理
+	//[詳細]　「オムライス」を代入する。
+		String food8 = "オムライス"; //好きな食べ物
+		System.out.println("好きな食べ物は" + food8 + "です");
+			
+	//[概要]　変数を入力し計算処理
+	//[詳細]　BMI＝体重(kg)　÷　身長(m)の2乗　になるように代入する。小数点第一位までしか表示させない。
+		System.out.println("BMIは" + String.format("%.1f", (weight8 / Math.pow((height8 / 100), 2))) + "です");
+	
+		
+		
+	//Qes9
+		
+	//[概要]　文字列型に代入処理
+	//[詳細]　「鈴木一郎」を代入する。
+		String name9 = "鈴木一郎"; //名前
+		System.out.println("初めまして" + name9 + "です");
+				
+	//[概要] バイト型に代入処理
+	//[詳細]　24を代入する。24を追加代入し和算する。
+		byte old9 = 24; //年齢
+		byte old91 = 24; //年齢
+		System.out.println("年齢は" + (old9 + old91)+ "歳です");
+					
+	//[概要]　単精度浮動小数点型に代入処理
+	//[詳細]　168.5を代入する。168.5を追加代入する。1つの変数に和算し、まとめる。
+		float height9 = 168.5f; //身長
+		float height91 = 168.5f; //身長
+		float totalheight = height9 + height91;
+		System.out.println("身長は" + totalheight + "cmです");
+					
+	//[概要]　単精度浮動小数点型に代入処理
+	//[詳細]　64.2を代入する。64.2を追加代入する。1つの変数に和算し、まとめる。
+		float  weight9 = 64.2f; //体重
+		float weight91 = 64.2f; //体重	
+		float totalweight = weight9 + weight91;
+		System.out.println("体重は" + totalweight + "kgです");
+		
+	//[概要]　文字列型に代入処理
+	//[詳細]　「オムライス」を代入する。
+		String food9 = "オムライス"; //好きな食べ物
+		System.out.println("好きな食べ物は" + food9 + "です");
+				
+	//[概要]　変数を入力し計算処理
+	//[詳細]　BMI＝体重(kg)　÷　身長(m)の2乗　になるように代入する。小数点第二位までしか表示させない。
+		System.out.println("BMIは" + String.format("%.2f", (totalweight / Math.pow((totalheight / 100), 2))) + "です");
+	
+		
+		
+	//Qes10
+		
+	//[概要] バイト型に代入処理　三項演算子処理
+	//[詳細]　oldに代入された数字が25以上ならtrueを表示する。
+		byte old10 = 24; //年齢
+		String Num = (old10 >= 25) ? "true" : "false";
+		System.out.println(Num);
+	
+		
+		
+	//Qes11
+		
+	//[概要] バイト型、単精度浮動小数点型に代入処理　→文字列型に型変換処理
+	//[詳細]　それぞれの型を型変換する。繋げて出力する。
+		byte old11 = 24;  //年齢
+		float height11 = 168.5f; //身長
+		float  weight11 = 64.2f; //体重
+					
+		String old12 = String.valueOf(old11); //年齢を文字列型変換
+		String height12 = String.valueOf(height11); //身長を文字列型変換
+		String weight12 = String.valueOf(weight11); //体重を文字列型変換
+					
+		System.out.println( old12 + "・" + height12 + "・" + weight12 );
+	
+		
+		
+	//Qes12
+		
+	//[概要] バイト型、単精度浮動小数点型に代入処理　→文字列型に型変換処理　→整数型に型変換処理
+	//[詳細]　それぞれの型を型変換する。
+		byte old12_1 = 24;  // 年齢
+		float height12_1 = 168.5f; // 身長
+					
+	// 年齢を文字列型変換
+		String old1 = String.valueOf(old12_1); 
+		System.out.println(old1);
+					
+	// 年齢を整数型変換
+		int newOld = Integer.parseInt(old1); 
+		System.out.println(newOld);
+					
+	// 身長を文字列型変換		
+		String height1 = String.valueOf(height12_1); 
+		System.out.println(height1);
+					
+	// float に変換してから int にキャスト	
+		float newHeight = Float.parseFloat(height1); 
+		int newHeightInt = (int) newHeight;  //小数点以下切り捨て
+		System.out.println(newHeightInt);
+	
+		
+		
+	//Qes13
+		
+	//[概要] バイト型、単精度浮動小数点型に代入処理　→文字列型に型変換処理　→整数型に型変換処理
+	//[詳細]　それぞれの型を型変換する。
+		byte old13 = 24;  // 年齢
+		float height13 = 168.5f; // 身長
+							
+	// 年齢を文字列型変換
+		String old13_1 = String.valueOf(old13); 
+		System.out.println(old13_1);
+							
+	// 年齢を整数型変換
+		int newOld13 = Integer.parseInt(old13_1); 
+		System.out.println(newOld13);
+							
+	// 身長を文字列型変換		
+		String height13_1 = String.valueOf(height13); 
+		System.out.println(height13_1);
+							
+	// float に変換してから int にキャスト	
+		float newHeight13 = Float.parseFloat(height13_1); 
+		int newHeightInt13 = (int) newHeight13;  //小数点以下切り捨て
+		System.out.println(newHeightInt13);
+				
+	//[概要] 比較演算子処理
+	//[詳細] 年齢が25もしくは身長が160以上の時 true にする。
+		String data = (newOld13 == 25 && newHeightInt13 >= 160) ? "true" : "false";
+		System.out.println(data);
+	}
+}
